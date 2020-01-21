e29bc0818bd991ae7fbd6af5b51768c52935f08d
Colin von Grimmenstein <colin@ifit.com>
2020-01-21 09:53:20 -0500

    Initial check-in of module TipCalc

---
+++ TipCalc/Services/CalculationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TipCalc.Core.Services
+{
+    public class CalculationService : ICalculationSerivce
+    {
+        public double TipAmount(double subTotal, int generosity)
+        {
+            return subTotal * generosity / 100.0;
+        }
+    }
+}
---
+++ TipCalc/Services/ICalculationService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace TipCalc.Core.Services
+{
+    public interface ICalculationSerivce
+    {
+        double TipAmount(double subTotal, int generosity);
+    }
+}
